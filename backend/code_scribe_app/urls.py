8aded8b7ddfd802a98ac31ed632c017089b791b8
Xiaoai Zhu <xzhu411@gatech.edu>
2025-02-22 21:27:43 -0500

    Update urls.py

--- backend/code_scribe_app/urls.py
+++ backend/code_scribe_app/urls.py
@@ -1,7 +1,14 @@
 # code_scribe_app/urls.py
+# from django.urls import path
+# from . import views
+
+
+
+# code_scribe_app/urls.py
 from django.urls import path
-from .views import markdown_view
+from .views import home, process_model_request  # 只保留用到的视图
 
 urlpatterns = [
-    path('markdown/<int:article_id>/', markdown_view, name='markdown_api'),
+    path('', home, name='home'),  # 主页
+    path('process/', process_model_request, name='process_model_request'),  # 处理模型请求
 ]
